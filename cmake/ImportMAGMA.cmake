5601aa6231bf60b038527fb693d286a46243cb5c
x_elshaha <x_elshaha@hen.kaust.edu.sa>
2026-02-05 15:52:40 +0300

    integrate MAGMA v2.9.0 from GitHub repository

--- cmake/ImportMAGMA.cmake
+++ cmake/ImportMAGMA.cmake
@@ -18,20 +18,48 @@ set(tag "v2.9.0")
 # 'version' specifies the version of the MAGMA library.
 set(version "2.9.0")
 # 'flag' is intended for additional configuration options during the build process.
-set(flag "-DGPU_TARGET=Ampere")
+set(flag "-DGPU_TARGET=Volta")
 # 'is_cmake' indicates that MAGMA uses CMake for its build system, which is set to ON.
 set(is_cmake ON)
-# 'is_git' - Using tarball (OFF) instead of git because git requires running 'make generate' first
-set(is_git OFF)
+# 'is_git' - Using GitHub repository
+set(is_git ON)
 # 'auto_gen' signals whether autogen scripts are required for the build process.
 set(auto_gen OFF)
- # 'url' provides the location of the MAGMA source tarball (v2.9.0 release).
-set(url "https://icl.utk.edu/projectsfiles/magma/downloads/magma-2.9.0.tar.gz")
+ # 'url' provides the location of the MAGMA GitHub repository.
+set(url "https://github.com/icl-utk-edu/magma.git")
 
-# The 'ImportDependency' macro script, located in the 'macros' directory, is included for managing the import and setup of the MAGMA library.
+# Define MAGMA-specific hooks for BuildDependency
+
+# Pre-build: Generate CMake files for git builds
+macro(MAGMA_PreBuild srcpath flags)
+    message(STATUS "MAGMA: Running make generate for git build")
+    string(REGEX MATCH "GPU_TARGET=([A-Za-z0-9_,]+)" _ "${flags}")
+    set(gpu "${CMAKE_MATCH_1}")
+    if(NOT gpu)
+        set(gpu "Volta")
+    endif()
+    file(WRITE "${srcpath}/make.inc" "BACKEND = cuda\nFORT = true\nGPU_TARGET = ${gpu}\n")
+    execute_process(COMMAND make generate WORKING_DIRECTORY ${srcpath} 
+        RESULT_VARIABLE result OUTPUT_QUIET ERROR_QUIET)
+    if(result)
+        message(FATAL_ERROR "MAGMA make generate failed")
+    endif()
+endmacro()
+
+# Post-build: Fix version in pkgconfig file
+macro(MAGMA_PostBuild installpath tag)
+    set(pc "${installpath}/lib/pkgconfig/magma.pc")
+    if(EXISTS "${pc}")
+        string(REPLACE "v" "" ver "${tag}")
+        file(READ "${pc}" content)
+        string(REPLACE "Version: 0.0.0" "Version: ${ver}" content "${content}")
+        file(WRITE "${pc}" "${content}")
+        message(STATUS "MAGMA: Fixed pkgconfig version to ${ver}")
+    endif()
+endmacro()
+
+# Use standard ImportDependency (will call our hooks automatically)
 include(macros/ImportDependency)
-# The 'ImportDependency' macro is invoked with the above-defined parameters to handle the detection, fetching, and integration of MAGMA into the project.
 ImportDependency(${name} ${tag} ${version} ${url} "${flag}" "" ${is_cmake} ${is_git} ${auto_gen})
 
-# A status message is outputted to indicate the successful integration of the MAGMA library into the project.
 message(STATUS "${name} done")
